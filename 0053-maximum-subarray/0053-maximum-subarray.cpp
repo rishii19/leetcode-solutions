a6418c61f8ab2c9d2ad3700cde196d43f9b5ebe3
Rishabh Yadav <88788649+rishii19@users.noreply.github.com>
2022-11-01 22:33:16 +0530

    Time: 258 ms (46.11%), Space: 67.7 MB (54.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int sum =0;
-        int maxi = INT_MIN;
+        int n = nums.size();
         
-        for(auto it:nums)
-        {
-            sum += it;
-            maxi = max(sum,maxi);
-            if(sum < 0) sum =0;
+        int maxi = nums[0];
+        int start = 0, end = 0; // the final start and end position of the maximum sum subarray
+        
+        int sum = 0;
+        int s = 0; // the temporary start position
+        
+        for(int i=0;i<n;i++){
+            
+            sum += nums[i];
+      
+            
+            if(sum > maxi){
+                maxi = sum;
+                start = s;
+                end = i;
+            }
+            
+            if(sum < 0){
+                sum = 0;
+                s = i+1;
+            }   
         }
-        return maxi;
+        
+        cout<<"Maximum Sum Subarray from nums["<<start<<"] = "<<nums[start]<<" till nums["<<end<<"] = "<<nums[end]<<endl;
+        
+        return maxi; 
     }
 };
