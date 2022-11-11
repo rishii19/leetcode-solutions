e01629ccaa605df1c1ede2e24cc6fcd6555bdaad
Rishabh Yadav <88788649+rishii19@users.noreply.github.com>
2022-11-11 18:31:15 +0530

    Time: 36 ms (9.33%), Space: 7.4 MB (13.03%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,9 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
         
-         return s == t;
+        //hashtable solution
+        
+        if(s.length() != t.length()) return false;
+        unordered_map<char, int> map;
+        
+        for(int i=0;i<s.length();i++){
+            map[s[i]]++;
+            map[t[i]]--;
+        }
+        for(auto it:map){
+            if(it.second>0) return false;
+        }
+        return true;
     }
 };
